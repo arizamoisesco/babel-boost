8b1216ca838bfdfc60ccf8ad6c296c0204caa6f2
Moises Ariza <arizamoises@gmail.com>
2023-06-22 22:49:57 -0500

    Create upload template and save to audio in this folder

--- audiototext/urls.py
+++ audiototext/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import index, upload_file
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", index, name="index"),
+    path('upload/', upload_file)
 ]
--- audiototext/views.py
+++ audiototext/views.py
@@ -3,16 +3,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadFileForm
 
 import whisper
+import os
 
 def upload_file(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES["files"])
-            return HttpResponseRedirect("")
+            file = form.cleaned_data['file']
+            handle_uploaded_file(file)#Función para guardar el archivo en la carpeta
+            return render(request, 'speechtext/upload_success.html')
+    else:
+        form = UploadFileForm()
+    
+    return render(request, 'speechtext/uploadForm.html', {'form': form})
         
 def handle_uploaded_file(file):
-    with open("")as destination:
+    #Obtener la ruta completa de la carpeta donde deseo guardarla
+    folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'audios')
+
+    #Verificar si la carpeta existe de lo contrario crearla
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    #Obtener la ruta completa del archivo a guardar
+    file_path = os.path.join(folder_path, file.name)
+    
+    with open(file_path, 'wb')as destination:
         for chunk in file.chunks():
             destination.write(chunk)
 
