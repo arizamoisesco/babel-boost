3563d9d6b6bc2cea9a49d94ca97c60f088727e61
Moises Ariza <arizamoises@gmail.com>
2023-06-27 22:05:01 -0500

    Create app account and manual view for login

--- babelBoost/urls.py
+++ babelBoost/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('audiototext/', include("audiototext.urls")),
+    path('account/', include("account.urls")),
     path('admin/', admin.site.urls),
 ]
