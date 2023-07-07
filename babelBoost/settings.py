a808e080c8d64146cb256cda4d627eb399f2fa4a
Moises Ariza <arizamoises@gmail.com>
2023-07-06 22:00:54 -0500

    Fix: Correction css style

--- babelBoost/settings.py
+++ babelBoost/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'babelBoost.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
